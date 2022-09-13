16b1fb479237266333fcb447122bc69898ddc643
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-09-13 13:36:35 -0400

    added webpack loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,40 @@ const path = require("path");
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
 };
